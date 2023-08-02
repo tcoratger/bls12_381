341dfdfc3f338805000a9cda836e17f1d6b4bf53
Thomas Coratger <thomas.coratger@gmail.com>
2023-08-02 12:19:51 +0200

    feat(test): add tests for mac

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -1,7 +1,5 @@
 import unittest
-from src.utils import (
-    sbb,
-)
+from src.utils import sbb, mac
 
 
 class TestSbb(unittest.TestCase):
@@ -38,6 +36,46 @@ class TestSbb(unittest.TestCase):
     def test_no_borrow(self):
         self.assertEqual(
             sbb(0x1111111111111111, 0x1111111111111111, 0x0),
+            (0x0, 0x0),
+        )
+
+
+class TestMac(unittest.TestCase):
+    # Test cases with no carry
+    def test_no_carry(self):
+        self.assertEqual(
+            mac(0x123456789ABCDEF0, 0x23456789ABCDEF12, 0x3456789ABCDEF012, 0x0),
+            (0x5BB3283248F48E34, 0x73602F699740C64),
+        )
+        self.assertEqual(
+            mac(0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0x0),
+            (0x0, 0xFFFFFFFFFFFFFFFF),
+        )
+
+    # Test cases with carry
+    def test_with_carry(self):
+        self.assertEqual(
+            mac(0x123456789ABCDEF0, 0x23456789ABCDEF12, 0x3456789ABCDEF012, 0x1),
+            (0x5BB3283248F48E35, 0x73602F699740C64),
+        )
+        self.assertEqual(
+            mac(
+                0x1111111111111111,
+                0x1111111111111111,
+                0x1111111111111111,
+                0xFFFFFFFFFFFFFFFF,
+            ),
+            (0x20FEDCBA98765431, 0x123456789ABCDF1),
+        )
+
+    # Test case with zero input
+    def test_zero_input(self):
+        self.assertEqual(
+            mac(0x0, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0x0),
+            (0x1, 0xFFFFFFFFFFFFFFFE),
+        )
+        self.assertEqual(
+            mac(0x0, 0x0, 0x0, 0x0),
             (0x0, 0x0),
         )
 
