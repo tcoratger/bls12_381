84e4bb0eb66f6c40e4282398b1ee7724266d0fde
Thomas Coratger <thomas.coratger@gmail.com>
2023-08-13 20:34:50 +0200

    feat(fp2): add lexicographically_largest implementation and unit tests

--- src/fp2.py
+++ src/fp2.py
@@ -11,6 +11,10 @@ class Fp2:
     def zero():
         return Fp2(Fp.zero(), Fp.zero())
 
+    @staticmethod
+    def one():
+        return Fp2(Fp.one(), Fp.zero())
+
     def is_zero(self):
         return self.c0.is_zero() and self.c1.is_zero()
 
@@ -27,3 +31,15 @@ class Fp2:
         # and because u^2 = -1, we get
         # (a - b) + (a + b)u
         return Fp2(self.c0 - self.c1, self.c0 + self.c1)
+
+    # Returns whether or not this element is strictly lexicographically
+    # larger than its negation.
+    def lexicographically_largest(self):
+        # If this element's c1 coefficient is lexicographically largest
+        # then it is lexicographically largest. Otherwise, in the event
+        # the c1 coefficient is zero and the c0 coefficient is
+        # lexicographically largest, then this element is lexicographically
+        # largest.
+        return self.c1.lexicographically_largest() or (
+            self.c1.is_zero() and self.c0.lexicographically_largest()
+        )
