0d620e3b27136c7024e0075db66c1f651d3407c2
Thomas Coratger <thomas.coratger@gmail.com>
2023-08-26 19:06:51 +0200

    feat(g1): add basic implementations and unit tests

--- src/g1.py
+++ src/g1.py
@@ -1,4 +1,13 @@
 from src.fp import Fp
+from src.utils import (
+    sbb,
+    mac,
+    adc,
+    wrapping_mul_u64,
+    wrapping_sub_u64,
+    CtOption,
+    Choice,
+)
 
 
 # This is an element of $\mathbb{G}_1$ represented in the affine coordinate space.
@@ -7,16 +16,52 @@ from src.fp import Fp
 # Values of `G1Affine` are guaranteed to be in the $q$-order subgroup unless an
 # "unchecked" API was misused.
 class G1Affine:
-    def __init__(self, x: Fp, y: Fp, inf: bool):
+    def __init__(self, x: Fp, y: Fp, inf: Choice):
         self.x = x
         self.y = y
         self.infinity = inf
 
+    def __neg__(self):
+        return self.neg()
+
     def identity():
-        return G1Affine(Fp.zero(), Fp.one(), True)
+        return G1Affine(Fp.zero(), Fp.one(), Choice(1))
 
     def default():
         return G1Affine.identity()
+
+    def conditional_select(a, b, choice: Choice):
+        return G1Affine(
+            Fp.conditional_select(a.x, b.x, choice),
+            Fp.conditional_select(a.y, b.y, choice),
+            b.infinity if choice.value else a.infinity,
+        )
+
+    # The only cases in which two points are equal are
+    # 1. infinity is set on both
+    # 2. infinity is not set on both, and their coordinates are equal
+    def eq(self, other):
+        return (self.infinity and other.infinity) or (
+            not self.infinity
+            and not other.infinity
+            and self.x.eq(other.x)
+            and self.y.eq(other.y)
+        )
+
+    def from_g1_projective(p):
+        zinv = p.z.invert()
+        if zinv.choice:
+            zinv = zinv.value
+        else:
+            zinv = Fp.zero()
+        x = p.x * zinv
+        y = p.y * zinv
+
+        tmp = G1Affine(x, y, Choice(0))
+
+        return G1Affine(
+            tmp, G1Affine.identity(), Choice(1) if zinv.is_zero() else Choice(0)
+        )
 
 
 # This is an element of $\mathbb{G}_1$ represented in the projective coordinate space.
@@ -31,3 +76,10 @@ class G1Projective:
 
     def default():
         return G1Projective.identity()
+
+    def conditional_select(a, b, choice: Choice):
+        return G1Projective(
+            Fp.conditional_select(a.x, b.x, choice),
+            Fp.conditional_select(a.y, b.y, choice),
+            Fp.conditional_select(a.z, b.z, choice),
+        )
---
+++ src/test_g1.py
@@ -0,0 +1,63 @@
+import unittest
+from src.fp import (
+    Fp,
+)
+from src.fp2 import (
+    Fp2,
+)
+from src.fp6 import (
+    Fp6,
+)
+from src.fp12 import (
+    Fp12,
+)
+from src.g1 import G1Affine, G1Projective
+import random
+from src.utils import array_to_number, Choice
+
+
+class TestG1(unittest.TestCase):
+    def test_identity(self):
+        a = G1Affine.identity()
+        self.assertTrue(a.x.is_zero() and a.y.eq(Fp.one()) and a.infinity.value == 1)
+
+    def test_default(self):
+        a = G1Affine.default()
+        self.assertTrue(a.eq(G1Affine.identity()))
+
+
+class TestConditionnalSelect(unittest.TestCase):
+    def test_conditional_select(self):
+        a = Fp(
+            [
+                0xDC90_6D9B_E3F9_5DC8,
+                0x8755_CAF7_4596_91A1,
+                0xCFF1_A7F4_E958_3AB3,
+                0x9B43_821F_849E_2284,
+                0xF575_54F3_A297_4F3F,
+                0x085D_BEA8_4ED4_7F79,
+            ]
+        )
+        b = Fp(
+            [
+                0xA1FA_FFFF_FFFE_5557,
+                0x995B_FFF9_76A3_FFFE,
+                0x03F4_1D24_D174_CEB4,
+                0xF654_7998_C199_5DBD,
+                0x778A_468F_507A_6034,
+                0x0205_5993_1F7F_8103,
+            ]
+        )
+
+        c = G1Affine(a, b, Choice(1))
+        d = G1Affine(b, a, Choice(0))
+
+        case0 = G1Affine.conditional_select(c, d, Choice(0))
+        case1 = G1Affine.conditional_select(c, d, Choice(1))
+
+        self.assertTrue(
+            case0.x.eq(c.x) and case0.y.eq(c.y) and case0.infinity.value == 1
+        )
+        self.assertTrue(
+            case1.x.eq(d.x) and case1.y.eq(d.y) and case1.infinity.value == 0
+        )
