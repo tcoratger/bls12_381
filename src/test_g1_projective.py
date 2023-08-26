141b743ad785be112a588b7e9054162925b2d406
Thomas Coratger <thomas.coratger@gmail.com>
2023-08-26 19:44:23 +0200

    feat(g1): improve unit test test_is_on_curve for G1Projective

--- src/test_g1_projective.py
+++ src/test_g1_projective.py
@@ -30,3 +30,20 @@ class TestIsOnCurve(unittest.TestCase):
     def test_is_on_curve(self):
         self.assertTrue(G1Projective.identity().is_on_curve())
         self.assertTrue(G1Projective.generator().is_on_curve())
+
+        z = Fp(
+            [
+                0xBA7A_FA1F_9A6F_E250,
+                0xFA0F_5B59_5EAF_E731,
+                0x3BDC_4776_94C3_06E7,
+                0x2149_BE4B_3949_FA24,
+                0x64AA_6E06_49B2_078C,
+                0x12B1_08AC_3364_3C3E,
+            ]
+        )
+
+        gen = G1Affine.generator()
+        test = G1Projective(gen.x * z, gen.y * z, z)
+        self.assertTrue(test.is_on_curve())
+        test.x = z
+        self.assertFalse(test.is_on_curve())
